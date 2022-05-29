5354521659ec8989988fabb8bb961de64e6f4886
Cloud Underground <10262311-Cloud-Underground@users.noreply.gitlab.com>
2022-05-29 23:21:49 +0000

    Update Production Artifacts/firefox-homepage.sh

--- Production Artifacts/firefox-homepage.sh
+++ Production Artifacts/firefox-homepage.sh
@@ -2,5 +2,5 @@
 sudo mkdir /config
 sudo mkdir /config/.mozilla
 sudo wget https://github.com/Underground-Ops/underground-nexus/raw/main/Production%20Artifacts/firefox.zip -O /config/.mozilla/firefox.zip
-sudo unzip /config/.mozilla/firefox.zip
+sudo unzip /config/.mozilla/firefox.zip /config/.mozilla/firefox
 sudo chmod -R a+rwx /config/.mozilla/firefox
