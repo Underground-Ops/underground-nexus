a6578f84aef02f054db766bdd17326e59a5846ea
Cloud Underground <10262311-Cloud-Underground@users.noreply.gitlab.com>
2022-10-24 03:36:12 +0000

    Update underground-nexus-update.sh

--- underground-nexus-update.sh
+++ underground-nexus-update.sh
@@ -5,3 +5,7 @@ dagger project update
 dagger do build
 rm -r /usr/local/bin/underground-nexus-update.sh
 cp /nexus-bucket/underground-nexus/underground-nexus-update.sh /usr/local/bin/
+cp /nexus-bucket/underground-nexus/underground-nexus-update.sh /nexus-bucket
+apt-get update -y && apt-get upgrade -y && apt-get dist-upgrade -y
+apt-get autoclean -y && apt-get clean -y
+apt-get autoremove
