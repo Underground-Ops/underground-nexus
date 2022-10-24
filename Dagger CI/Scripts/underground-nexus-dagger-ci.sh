0b5385faaa148e97962ad5e0e5a09676bfd3c0ad
Cloud Underground <10262311-Cloud-Underground@users.noreply.gitlab.com>
2022-10-24 00:24:18 +0000

    Update Dagger CI/Scripts/underground-nexus-dagger-ci.sh

---
+++ Dagger CI/Scripts/underground-nexus-dagger-ci.sh
@@ -0,0 +1,8 @@
+#This script builds a portable Dagger CI pipeline that is preconfigured for CICD integration with the Underground Nexus Copy Paste Data Center and Software Factory or any other Docker based deployment (Dagger works with most CI piplines, check out the Dagger website to learn more at https://dagger.io)
+#-------------------------------
+#apt install git
+git clone https://github.com/Underground-Ops/underground-nexus.git
+cd underground-nexus/
+dagger project init
+dagger project update
+#dagger do build
