011e12b47efa0dc3e67b857539d1c2ff84797ca0
Cloud Underground <10262311-Cloud-Underground@users.noreply.gitlab.com>
2022-10-26 06:41:41 +0000

    Update Dagger CI/Scripts/underground-nexus-dagger-ci.sh

--- Dagger CI/Scripts/underground-nexus-dagger-ci.sh
+++ Dagger CI/Scripts/underground-nexus-dagger-ci.sh
@@ -14,14 +14,14 @@ bash install.sh
 #Install kubectl
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl" && chmod +x ./kubectl && mv ./kubectl /usr/local/bin/kubectl
 #Install HELM with the standard stable repository and GitLab repository
-curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash && helm repo add stable https://charts.helm.sh/stable && helm repo add gitlab https://charts.gitlab.io/
+curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash && helm repo add stable https://charts.helm.sh/stable && helm repo add gitlab https://charts.gitlab.io/ $$ rm /install.sh
 #Configure the Underground Nexus automated weekly update scheduling kit
 mv underground-nexus-dagger-ci.sh old-underground-nexus-dagger-ci.sh
 wget -O re-initialize-dagger-ci.sh https://raw.githubusercontent.com/Underground-Ops/underground-nexus/main/Dagger%20CI/Scripts/underground-nexus-dagger-ci.sh
 sh re-initialize-dagger-ci.sh
 #Cleanup
-rm /install.sh
-rm /install.sh.1
+#rm /install.sh
+#rm /install.sh.1
 #Prepare Update Management
 sh /nexus-bucket/underground-nexus/underground-nexus-update.sh
 #Use crontab to schedule updates for Sundays if Athena0 has a docker socket
