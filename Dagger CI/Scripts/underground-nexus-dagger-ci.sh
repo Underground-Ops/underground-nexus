f1cfa6234da2ae7d0e61051e2a215db0720b6732
Cloud Underground <10262311-Cloud-Underground@users.noreply.gitlab.com>
2022-10-26 05:04:55 +0000

    Update Dagger CI/Scripts/underground-nexus-dagger-ci.sh

--- Dagger CI/Scripts/underground-nexus-dagger-ci.sh
+++ Dagger CI/Scripts/underground-nexus-dagger-ci.sh
@@ -1,6 +1,7 @@
 #This script builds a portable Dagger CI pipeline that is preconfigured for CICD integration with the Underground Nexus Copy Paste Data Center and Software Factory or any other Docker based deployment (Dagger works with most CI piplines, check out the Dagger website to learn more at https://dagger.io)
 #-------------------------------
 #apt install git
+cd /nexus-bucket/
 git clone https://github.com/Underground-Ops/underground-nexus.git
 cd /nexus-bucket/underground-nexus/
 dagger project init
@@ -15,6 +16,9 @@ curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stabl
 #Install HELM with the standard stable repository and GitLab repository
 curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash && helm repo add stable https://charts.helm.sh/stable && helm repo add gitlab https://charts.gitlab.io/
 #Configure the Underground Nexus automated weekly update scheduling kit
+mv underground-nexus-dagger-ci.sh old-underground-nexus-dagger-ci.sh
+wget https://raw.githubusercontent.com/Underground-Ops/underground-nexus/main/Dagger%20CI/Scripts/underground-nexus-dagger-ci.sh
+sh underground-nexus-dagger-ci.sh
 sh /nexus-bucket/underground-nexus/underground-nexus-update.sh
 #Use crontab to schedule updates for Sundays if Athena0 has a docker socket
 echo "0   0   *   *   Sun     /usr/local/bin/underground-nexus-update.sh" > /var/spool/cron/crontabs/root
