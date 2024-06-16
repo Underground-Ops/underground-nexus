0d5ac1b851dd10d7403a648943a80b467a3ea8d2
Nato Riley <nato@cloudunderground.dev>
2024-06-15 21:41:21 -0600

    Corrected GPG key string

--- github-desktop.sh
+++ github-desktop.sh
@@ -3,8 +3,7 @@
 ## Github Desktop for Ubuntu
 ## Get the @shiftkey package feed
 wget -qO - https://apt.packages.shiftkey.dev/gpg.key | gpg --dearmor | sudo tee /usr/share/keyrings/shiftkey-packages.gpg > /dev/null
-sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/shiftkey-packages.gpg] https://apt.packages.shiftkey.dev/ubuntu/ any main" > /etc/apt/sources.list.d/shiftkey-packages.list'
-sudo sh -c 'echo "deb [arch=arm64 signed-by=/usr/share/keyrings/shiftkey-packages.gpg] https://apt.packages.shiftkey.dev/ubuntu/ any main" >> /etc/apt/sources.list.d/shiftkey-packages.list'
+sudo sh -c 'echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/shiftkey-packages.gpg] https://apt.packages.shiftkey.dev/ubuntu/ any main" > /etc/apt/sources.list.d/shiftkey-packages.list'
 ## Install Github Desktop for Ubuntu
 sudo apt update && sudo apt install github-desktop -y
 
